ddc5d69622dcef24303b758a9d9734b525148e0c
Rifat <mdrifat107590@gmail.com>
2026-05-26 18:53:23 +0600

    connected with mongodb

--- server.js
+++ server.js
@@ -1,18 +1,38 @@
 const express = require("express");
 const cors = require("cors");
-require("dotenv").config();
+const dotenv = require("dotenv");
+const { MongoClient,ServerApiVersion  } = require("mongodb");
+dotenv.config();
 const app = express();
-const port = process.env.PORT || 5000;
-
 app.use(cors());
 
 app.use(express.json());
-
-
-app.get("/", (req, res) => {
-  res.send("Pet Heaven Server Running");
+const port = process.env.PORT || 5000;
+const uri = process.env.MONGO_URI;
+const client = new MongoClient(uri, {
+  serverApi: {
+    version: ServerApiVersion.v1,
+    strict: true,
+    deprecationErrors: true,
+  },
 });
+async function run() {
+  try {
+    const database = client.db("pawHavenDB");
+    const petsCollection = database.collection("pets");
+    const requestsCollection = database.collection("requests");
+    const usersCollection = database.collection("users");
 
+    app.get("/", (req, res) => {
+      res.send("PawHaven Server Running...");
+    });
+    await client.connect();
+    console.log("MongoDB Connected Successfully");
+  } catch (error) {
+    console.log(error);
+  }
+}
+run();
 
 app.listen(port, () => {
   console.log(`Server running on port ${port}`);
