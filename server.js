fc37ef9aea6a6f903e0896bc8310140920c9c2e7
Rifat <mdrifat107590@gmail.com>
2026-05-28 13:15:14 +0600

    fixed origins

--- server.js
+++ server.js
@@ -16,7 +16,7 @@ const app = express();
 
 app.use(
   cors({
-    origin: ["https://pet-heaven-a9.netlify.app"],
+    origin: ["http://localhost:5173", "https://pet-heaven-a9.netlify.app"],
 
     credentials: true,
   }),
